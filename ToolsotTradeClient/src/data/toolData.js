e118a29a40cfcf468ee13fc299039f7261f0fe18
Luke Allen <lukus2013@yahoo.com>
2022-06-11 11:32:24 -0500

    data

--- ToolsotTradeClient/src/data/toolData.js
+++ ToolsotTradeClient/src/data/toolData.js
@@ -1,3 +1,65 @@
 import axios from "axios";
 
 const dbURL = "https://localhost:7008/api";
+
+const getTools = () =>
+    new Promise((resolve, reject) => {
+        axios
+            .get(`${dbURL}/Tool`)
+            .then((response) => resolve(Object.values(response.data)))
+            .catch(reject);
+    });
+
+const getToolById = () =>
+    new Promise((resolve, reject) => {
+        axios.get(`${dbURL}/Tool/id/${id}`)
+        .then(resolve)
+        .catch(reject);
+    });
+
+const getToolByName = () =>
+    new Promise((resolve, reject) => {
+        axios.get(`${dbURL}/Tool/name/${name}`)
+        .then(resolve)
+        .catch(reject);
+    });
+
+const getToolsByType = () =>
+    new Promise((resolve, reject) => {
+        axios.get(`${dbURL}/Tool/type/${type}`)
+        .then(resolve)
+        .catch(reject);
+    });
+
+const getToolsByManufacturer = () =>
+    new Promise((resolve, reject) => {
+        axios.get(`${dbURL}/Tool/manufacturer/${manufacturer}`)
+        .then(resolve)
+        .catch(reject);
+    });
+
+const addNewTool = (toolObj) => new Promise((resolve, reject) => {
+    axios
+    .post(`${dbUrl}/Tool`, toolObj)
+    .then(resolve)
+    .catch(reject);
+});
+
+const updateTool = (toolObj, obj) => new Promise((resolve, reject) => {
+    axios
+    .put(`${dbURL}/Tool/${id}`, obj)
+    .then(() => getTools(),then(resolve))
+    .catch(reject);
+})
+
+const deleteTool = (id) => new Promise((resolve, reject) => {
+    axios
+    .delete(`${dbURL}/Tool/${id}`)
+    .then(() => getTools().then(resolve))
+    .catch(reject);
+});
+
+export {
+    getTools, getToolById, getToolByName, getToolsByType, getToolsByManufacturer, addNewTool, updateTool, deleteTool
+};
+
